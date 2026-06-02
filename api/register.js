7fd017ab9026631539ba8a4ec3c1340ae6e23f88
kuramavenom-v1 <opaidamagdy1234@gmail.com>
2026-06-03 00:25:24 +0300

    update vm system + fix api + ui improvements

--- api/register.js
+++ api/register.js
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
+function generateId() {
+  const random5 = Math.floor(10000 + Math.random() * 90000);
+  return `388${random5}`;
+}
+
 export default async function handler(req, res) {
+
   const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_ANON_KEY
@@ -8,10 +14,26 @@ export default async function handler(req, res) {
 
   const { name, age, city, psn, image_url } = req.body;
 
+  if (!name) {
+    return res.status(400).json({
+      success: false,
+      error: "Missing data"
+    });
+  }
+
+  const userId = generateId();
+
   const { data, error } = await supabase
     .from("players")
     .insert([
-      { name, age, city, psn, image_url }
+      {
+        id: userId,
+        name,
+        age,
+        city,
+        psn,
+        image_url
+      }
     ])
     .select()
     .single();
